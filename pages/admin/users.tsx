08f7c7a51a2bd3955057161819c51ab511e471ca
Armando Murillo <totovip1464@gmail.com>
2023-03-03 22:57:59 -0600

    users.tsx: Refactorizacion a la page

--- pages/admin/users.tsx
+++ pages/admin/users.tsx
@@ -1,48 +1,16 @@
-import { useEffect, useState } from 'react'
 import { PeopleOutline } from '@mui/icons-material'
 import { Grid, MenuItem, Select } from '@mui/material'
 
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid'
-import useSWR from 'swr'
 
 import { AdminLayout } from '../../components/layouts'
-import { IUser } from '../../interfaces'
-import tesloApi from '../../api/tesloApi';
+import { useUsers } from '../../hooks'
 
 const UsersPage = () => {
 
-    const { data, error } = useSWR<IUser[]>('/api/admin/users');
-    const [ users, setUsers ] = useState<IUser[]>([]);
-
-    useEffect(() => { //Esto lo hacemos para que cada que cambie la data el useEffect se ejecute y llene de nuevo el estado con el update
-        //Esto evita que el usuario tenga que recargar la página para ver los cambios
-      if( data ) {
-        setUsers(data)
-      }
-    }, [data])
-    
+    const { users, data, error, onRoleUpdate } = useUsers();
 
     if( !data && !error ) return (<></>);
-
-    const onRoleUpdate = async( userId: string, newRole: string ) => {
-
-        const previousUsers = users.map( user => ({ ...user }) ); //Copia de la información antes del cambio por si hay un error
-        const updatedUsers = users.map( user => ({
-            ...user,
-            role: userId === user._id ? newRole : user.role
-        }) );
-
-        setUsers(updatedUsers);
-
-        try {
-            await tesloApi.put('/admin/users', { userId, role: newRole }) //Esos dos parametros ya que es lo que pide el API
-        } catch (error) {
-            setUsers(previousUsers);
-            console.log(error);
-            alert('No se pudo actualizar el role del usuario')
-        } 
-
-    }
 
     const columns: GridColDef[] = [
         { field: 'email', headerName: 'Correo', width: 250 },
