8f950ce2e655334d6718786b8f51f76aae11f2cc
ArmandoCode <totovip1464@gmail.com>
2023-06-04 14:43:43 -0600

    Nuevo componente en la página principal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
+import { useState } from 'react';
 import type { NextPage } from 'next'
 import { Typography } from '@mui/material'
 
 import { ShopLayout } from '../components/layouts'
 import { ProductList } from '../components/products';
 import { useProducts } from '../hooks';
-import { FullScreenLoading } from '../components/ui';
+import { FullScreenLoading, DialogComponent } from '../components/ui';
 
 const HomePage: NextPage = () => {
 
@@ -15,6 +16,8 @@ const HomePage: NextPage = () => {
       <Typography variant='h1' component='h1'>Store</Typography> 
       {/* "component" le indica al SEO que es el titulo de la pagina */}
       <Typography variant='h2' sx={{ mb: 1 }}>All the products</Typography>
+
+      <DialogComponent />
 
       {
         isLoading
